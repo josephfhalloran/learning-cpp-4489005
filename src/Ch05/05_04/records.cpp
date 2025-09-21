7420a71e59652d9e5740520c8a98b5ab1e5fd9a0
Joseph Halloran <103540785+josephfhalloran@users.noreply.github.com>
2025-09-21 22:19:01 +0000

    fin ch 5

--- src/Ch05/05_04/records.cpp
+++ src/Ch05/05_04/records.cpp
@@ -1,4 +1,5 @@
 #include "records.h"
+#include <iostream>
 
 Student::Student(int the_id, std::string the_name){
     id = the_id;
@@ -84,7 +85,12 @@ unsigned char StudentRecords::get_course_credits(int cid) const{
         j++;
     return courses[j].get_credits();
 }
-
+std::string StudentRecords::get_course_name(int cid) const{
+    int j = 0;
+    while (j < courses.size() && courses[j].get_id() != cid)
+        j++;
+    return courses[j].get_name();
+}
 float StudentRecords::get_GPA(int sid) const{
     float points = 0.0f, credits = 0.0f;
     for (const Grade& grd : grades)
@@ -94,4 +100,11 @@ float StudentRecords::get_GPA(int sid) const{
             points += get_num_grade(grd.get_grade()) * current_credits;
         }
     return (points / credits);
+}
+void StudentRecords::report_card(int sid) {
+    std::cout << "Report card for " << get_student_name(sid) << std::endl;
+    for (const Grade& grd : grades)
+        if(grd.get_student_id() == sid)
+            std::cout << get_course_name(grd.get_course_id()) << ": " << grd.get_grade() << std::endl;
+    std::cout << "GPA: " << get_GPA(sid) << std::endl;
 }
